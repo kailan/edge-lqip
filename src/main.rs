bf383a6b1d41f2256c3e85d0106d9d37892a389a
Kailan Blanks <kailan@enviark.com>
2023-06-29 18:30:40 +0100

    Switch to `thumbhash`, decode hashes on client-side

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,113 @@
-use blurhash::{decode, encode};
+use fastly::cache::core::CacheKey;
 use fastly::http::{header, Method};
 use fastly::{mime, Error, Request, Response};
 use image::io::Reader as ImageReader;
-use image::{DynamicImage, GenericImageView, ImageBuffer};
-use std::io::Cursor;
+use image::GenericImageView;
+use std::io::{Cursor, Write};
+use std::time::Duration;
+use thumbhash::rgba_to_thumb_hash;
 
 const CONTENT_BACKEND: &str = "content_backend";
-const CONTENT_HOST: &str = "images.unsplash.com";
+
+const CACHE_DURATION_SECS: u64 = 31536000;
 
 // Generate a LQIP (low-quality image placeholder) from a given image
 fn lqip_generator(mut req: Request) -> Result<Response, Error> {
     // Strip /lqip from the path.
-    let image_path = &req.get_path()[5..].to_owned();
-    req.set_path(image_path);
+    let image_path = req.get_path()[5..].to_owned();
 
-    // The `q` parameter is specific to the image backend used in our example (unsplash.com),
-    // and is used to specify the image quality.
-    // We don't need a high quality version of the image to generate a blurry placeholder!
-    req.set_query_str(format!("{}&q=1", &req.get_query_str().unwrap_or_default()));
+    // Store a value to indicate whether the image was found in the cache.
+    let mut x_cache = "MISS";
 
-    // Retrieve a low-quality version of the image from the image backend.
-    let img_bytes = req.send(CONTENT_BACKEND)?.take_body_bytes();
+    // Check the cache for a previously generated LQIP.
+    let thumb_hash = if let Some(entry) =
+        fastly::cache::core::lookup(CacheKey::copy_from_slice(image_path.as_bytes()))
+            .execute()
+            .unwrap()
+    {
+        println!("cache hit");
+        x_cache = "HIT";
+        entry.to_stream().unwrap().into_bytes()
+    } else {
+        req.set_path(&image_path);
 
-    // Decode the image.
-    let img = ImageReader::new(Cursor::new(img_bytes))
-        .with_guessed_format()?
-        .decode()?;
+        // Apply any origin-specific transformations to the request.
+        req.set_query_str(format!("{}&q=1", &req.get_query_str().unwrap_or("?")));
 
-    // Get the image dimensions.
-    let (width, height) = &img.dimensions();
+        // Send the request for the image to the content backend.
+        let img_response = req.send(CONTENT_BACKEND)?;
 
-    // Generate a blurhash.
-    let blurhash = encode(7, 6, *width, *height, &img.to_rgba8().into_vec());
+        // Load the entire image into memory.
+        let img_bytes = img_response.into_body_bytes();
 
-    // Turn the blurhash into an array of pixels for our placeholder image.
-    let pixels = decode(&blurhash, *width, *height, 1.0);
+        // Decode the image data from the raw bytes.
+        let img = ImageReader::new(Cursor::new(img_bytes))
+            .with_guessed_format()?
+            .decode()?;
 
-    // Generate the LQIP.
-    let placeholder =
-        DynamicImage::ImageRgba8(ImageBuffer::from_vec(*width, *height, pixels).unwrap());
+        // Get the image dimensions.
+        let (width, height) = &img.dimensions();
 
-    // Encode it as a JPEG. Quality = 30 is plenty for our purposes.
-    let mut bytes: Vec<u8> = Vec::new();
-    placeholder.write_to(&mut bytes, image::ImageOutputFormat::Jpeg(30))?;
+        println!("b4 width {} height {}", width, height);
 
-    // Respond with the placeholder image and long cache directives.
-    Ok(Response::from_body(bytes)
-        .with_content_type(mime::IMAGE_JPEG)
-        .with_header(header::CACHE_CONTROL, "public, max-age=31536000, immutable")
-        .with_header("X-Blurhash", blurhash)
-        .with_header("X-Width", format!("{}", width))
-        .with_header("X-Height", format!("{}", height)))
+        // If the image is over 100px in either dimension, resize while maintaining aspect ratio.
+        let img = if width > height && width > &100 {
+            img.resize(
+                100,
+                (100f64 * (*height as f64) / (*width as f64)).floor() as u32,
+                image::imageops::FilterType::Nearest,
+            )
+        } else if height > width && height > &100 {
+            img.resize(
+                (100f64 * (*width as f64) / (*height as f64)).floor() as u32,
+                100,
+                image::imageops::FilterType::Nearest,
+            )
+        } else {
+            img
+        };
+
+        // Get the updated image dimensions.
+        let (width, height) = &img.dimensions();
+
+        println!("then width {} height {}", width, height);
+
+        // Generate a thumbhash.
+        let thumb_hash = rgba_to_thumb_hash(
+            (*width).try_into().unwrap(),
+            (*height).try_into().unwrap(),
+            &img.to_rgba8().into_vec(),
+        );
+
+        let mut writer = fastly::cache::core::insert(
+            CacheKey::copy_from_slice(image_path.as_bytes()),
+            Duration::from_secs(CACHE_DURATION_SECS),
+        )
+        .known_length(thumb_hash.len() as u64)
+        .execute()
+        .unwrap();
+        writer.write_all(&thumb_hash).unwrap();
+        writer.finish().unwrap();
+
+        thumb_hash
+    };
+
+    // Respond with the thumbhash.
+    Ok(Response::from_body(thumb_hash)
+        .with_content_type(mime::APPLICATION_OCTET_STREAM)
+        .with_header(header::CACHE_CONTROL, format!("public, max-age={}, immutable", CACHE_DURATION_SECS))
+        .with_header("X-Cache", x_cache))
 }
 
 #[fastly::main]
-fn main(mut req: Request) -> Result<Response, Error> {
-    // Set an override host header.
-    req.set_header(header::HOST, CONTENT_HOST);
+fn main(req: Request) -> Result<Response, Error> {
     // Pattern match on the request method and path.
     match (req.get_method(), req.get_path()) {
-        // Demo set-up (see https://developer.fastly.com/solutions/demos)
-        (&Method::GET, "/.well-known/fastly/demo-manifest") => {
-            Ok(Response::new().with_body_text_plain(include_str!("demo-manifest")))
-        }
-        (&Method::GET, "/screenshot.jpg") => Ok(Response::new()
-            .with_body_octet_stream(include_bytes!("screenshot.jpg"))
-            .with_content_type(mime::IMAGE_PNG)
-            .with_header(header::CACHE_CONTROL, "public, max-age=31536000, immutable")),
-        // If the request is a `GET` to the `/` path, serve a html page.
-        (&Method::GET, "/") => Ok(Response::new().with_body_text_html(include_str!("index.html"))),
-        
-        // If the request is a `GET` to the `/lqip/*` path, generate a LQIP (low-quality image placeholder).
+        // If the request is a `GET` to the `/lqip/*` path, generate a LQIP.
         (&Method::GET, path) if path.starts_with("/lqip/") => lqip_generator(req),
+        // If the request is a `GET` to the `/` path, serve a HTML page.
+        (&Method::GET, "/") => Ok(Response::new().with_body_text_html(include_str!("index.html"))),
         // Forward all other requests to the content backend.
-        _ => {
-            let mut beresp = req.send(CONTENT_BACKEND)?;
-            beresp.set_header(header::CACHE_CONTROL, "public, max-age=31536000, immutable");
-            Ok(beresp)
-        },
+        _ => Ok(req.send(CONTENT_BACKEND)?),
     }
 }
